a28bf2e347247c54249f8d57d6ee7be352a4a795
suyashshukla <suyashshukla1997@gmail.com>
2018-12-18 11:54:53 +0530

    Updated on 18th December

--- Java/CGM/src/Codechef/Friends.java
+++ Java/CGM/src/Codechef/Friends.java
@@ -16,8 +16,31 @@ public class Friends {
             int y = sc.nextInt();
             int z = sc.nextInt();
 
+            int a,b,c,sum;
+            boolean flag = false;
 
 
+            for(int i = 1;i<=2;i++) {
+                a = (int) Math.pow(-1, i) * x;
+                for (int j = 1; j <= 2; j++) {
+                    b = (int) Math.pow(-1, j) * y;
+                    for (int k = 1; k <= 2; k++) {
+                        c = (int) Math.pow(-1, k) * z;
+
+                        sum = a + b + c;
+
+                        if (sum == 0) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if(flag)
+                System.out.println("yes");
+            else
+                System.out.println("no");
 
             T--;
         }
